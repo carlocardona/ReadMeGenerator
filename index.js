aea72321fb770a24fbe0a8d1272c544d8bf5cd39
Carlo C <pbjunkiep3@yahoo.com>
2020-07-19 08:10:35 +1000

    added questions for readME

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const inquirer = require("inquirer");
+const inq = require("inquirer");
 const fs = require("fs");
+const util = require('util');
+
+inq.prompt([
+    {
+        type:"input",
+        message: ""
+
+    }
+])
+
+/*
+"What is your GitHub username?"
+"What is your email address?"
+"What is your project's name?"
+"Please write a short descripiton of your project."
+"What kind of license should your project have?"
+"What command should be run to install dependencies?"
+"What command should be run to run tests?"
+"What does the user need to know about using the repo?"
+"What does the user need to know about contributing to the repo?"
+*/
+
+
