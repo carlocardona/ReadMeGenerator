2d3bf335c854458d735988464cc5539919a809ae
Carlo C <pbjunkiep3@yahoo.com>
2020-07-19 08:25:14 +1000

    testing the then

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 const inq = require("inquirer");
-const fs = require("fs");
-const util = require('util');
+//const fs = require("fs");
+//const util = require('util');
 
 inq.prompt([
     {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
         type:"input",
-        message: ""
-
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "projName"
+    },
+    {
+        type: "input",
+        message: "Please write a short descripiton of your project.",
+        name: "desc"
+    },
+    {
+        type: "input",
+        message: "What kind of license should your project have?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message:"What command should be run to install dependencies?",
+        name:"depend"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "commands"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "info"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributer"
     }
-])
-
-/*
-"What is your GitHub username?"
-"What is your email address?"
-"What is your project's name?"
-"Please write a short descripiton of your project."
-"What kind of license should your project have?"
-"What command should be run to install dependencies?"
-"What command should be run to run tests?"
-"What does the user need to know about using the repo?"
-"What does the user need to know about contributing to the repo?"
-*/
+]).then(response => {
+    console.log(response);
+})
 
 
